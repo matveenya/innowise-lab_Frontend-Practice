8a5d59d1c429089f36d0642ff3df65551d2bda27
Roman Matveenya <romanmatveena@gmail.com>
2025-10-31 17:41:35 +0300

    working with validation in form components

--- app/components/form/PasswordInput.vue
+++ app/components/form/PasswordInput.vue
@@ -1,48 +1,69 @@
 <template>
   <div class="form-group">
-    <div class="password-wrapper">
-      <input type="password" placeholder="Password" class="form-input" />
-      <Icon name="mdi:eye" size="1.2em" class="password-icon" mode="svg" />
-    </div>
+    <Field v-slot="{ field, errorMessage }" :name="name">
+      <div class="password-wrapper">
+        <input
+          type="password"
+          placeholder="Password"
+          class="form-input"
+          :class="{ 'is-error': errorMessage }"
+          v-bind="field"
+        />
+        <Icon name="mdi:eye" size="1.2em" class="password-icon" mode="svg" />
+      </div>
+      <ErrorMessage :name="name" class="form-error" />
+    </Field>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
 
 <style scoped lang="scss">
 .form-group {
   text-align: left;
   width: 100%;
-}
 
-.form-input {
-  width: 100%;
-  padding: $space-md $space-lg;
-  border-radius: $radius-sm;
-  border: $border-outline;
-  background-color: transparent;
-  color: $color-text-primary;
-  font-size: $font-size-md;
+  .password-wrapper {
+    position: relative;
 
-  &:focus {
-    outline: none;
-    border: $border-outline-hover;
+    .form-input {
+      width: 100%;
+      padding: $space-md $space-lg;
+      border-radius: $radius-sm;
+      border: $border-outline;
+      background-color: transparent;
+      color: $color-text-primary;
+      font-size: $font-size-md;
+
+      &:focus {
+        outline: none;
+        border: $border-outline-hover;
+      }
+
+      &.is-error {
+        border-color: $color-secondary;
+      }
+    }
+
+    .password-icon {
+      position: absolute;
+      right: $space-lg;
+      top: 50%;
+      transform: translateY(-50%);
+      color: $color-text-primary;
+      z-index: 10;
+      cursor: pointer;
+    }
   }
-}
 
-.password-wrapper {
-  position: relative;
-
-  .form-input {
-    padding-right: $space-4xl;
-  }
-
-  .password-icon {
-    position: absolute;
-    right: $space-lg;
-    top: 50%;
-    transform: translateY(-50%);
-    color: $color-text-primary;
-    z-index: 10;
-    cursor: pointer;
+  .form-error {
+    color: $color-secondary;
+    font-size: $font-size-sm;
+    margin-top: $space-xs;
   }
 }
 </style>
---
+++ app/utils/schemas/authValidationSchema.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+const REQUIRED_EMAIL_MESSAGE = 'Email is required';
+const INVALID_EMAIL_MESSAGE = 'Email Incorrect';
+const REQUIRED_PASSWORD_MESSAGE = 'Password is required';
+const INVALID_PASSWORD_MESSAGE = 'Password should be at least 6 symbols';
+
+export const authSchema = z.object({
+  email: z
+    .string({ required_error: REQUIRED_EMAIL_MESSAGE })
+    .email(INVALID_EMAIL_MESSAGE)
+    .min(1, REQUIRED_EMAIL_MESSAGE),
+  password: z
+    .string({ required_error: REQUIRED_PASSWORD_MESSAGE })
+    .min(6, INVALID_PASSWORD_MESSAGE),
+});
+
+export const forgotPasswordSchema = z.object({
+  password: z
+    .string({ required_error: REQUIRED_PASSWORD_MESSAGE })
+    .min(6, INVALID_PASSWORD_MESSAGE),
+});
+
+export type AuthSchema = z.infer<typeof authSchema>;
+export type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
