55fb9f817e477f1d98b8507c802b8962416873f3
Victoria <dr.v.orlova@gmail.com>
2025-11-07 15:56:33 +0300

    fix: fix ui bug with positioning at authentification pages

--- app/components/auth/FormContainer.vue
+++ app/components/auth/FormContainer.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="auth-form-container" :class="{ 'auth-form-container--centered': isCentered }">
+    <div v-if="$slots.header" class="auth-form-container__header">
+      <slot name="header" />
+    </div>
+
     <div class="auth-form-container__content">
       <slot />
     </div>
@@ -14,12 +18,16 @@ defineProps<{
 
 <style scoped lang="scss">
 .auth-form-container {
-  @include d-flex(center, flex-start);
+  @include d-flex(flex-start, center, column);
   width: 100%;
   min-height: 100vh;
 
   &--centered {
-    @include d-flex(center, center);
+    @include d-flex(center, center, column);
+  }
+
+  &__header {
+    width: 100%;
   }
 
   &__content {
@@ -27,6 +35,7 @@ defineProps<{
     max-width: 560px;
     background-color: $color-primary;
     padding: $space-2xl 0;
+    margin-block: auto;
   }
 }
 </style>
